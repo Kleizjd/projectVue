ff84f070327fca78b77c6d56509589930268877d
E. David Riascos M <desarrollosoft.david96@gmail.com>
2023-04-12 15:09:06 -0500

    first commit

--- frontend/asamblea-virtual/src/components/dataTableComponent.vue
+++ frontend/asamblea-virtual/src/components/dataTableComponent.vue
@@ -1,140 +1,111 @@
 <template>
   <div class="container" id="table-content">
     <div class="table-container">
-      <b-table :items="items" :fields="fields">
+      <b-table :items="tableItems" :fields="fields">
         <template #cell(id)="data">
-          <!-- <b-form-input type="number" v-if="items[data.index].isEdit" v-model="items[data.index].id"></b-form-input> -->
+          <!-- <b-form-input type="number" v-if="tableItems[data.index].isEdit" v-model="tableItems[data.index].id"></b-form-input> -->
           <span>{{ data.value }}</span>
         </template>
         <template #cell(cedula)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="number" v-model="items[data.index].cedula"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="number"
+            v-model="tableItems[data.index].cedula"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(nombre)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="text" v-model="items[data.index].nombre"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="text"
+            v-model="tableItems[data.index].nombre"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(agencia)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="text" v-model="items[data.index].agencia"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="text"
+            v-model="tableItems[data.index].agencia"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(celular)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="number" v-model="items[data.index].celular"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="number"
+            v-model="tableItems[data.index].celular"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(estado_personas_id)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="text"
-            v-model="items[data.index].estado_personas_id"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="text"
+            v-model="tableItems[data.index].estado_personas_id"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(correo)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="text" v-model="items[data.index].correo"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="text"
+            v-model="tableItems[data.index].correo"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(grupo)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="number" v-model="items[data.index].grupo"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="number"
+            v-model="tableItems[data.index].grupo"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(rol_id)="data">
-          <b-form-input v-if="items[data.index].isEdit" type="number" v-model="items[data.index].rol_id"></b-form-input>
+          <b-form-input
+            v-if="tableItems[data.index].isEdit"
+            type="number"
+            v-model="tableItems[data.index].rol_id"
+          ></b-form-input>
           <span v-else>{{ data.value }}</span>
         </template>
         <template #cell(edit)="data">
-          <b-button pill variant="warning" size="sm" @click="editRowHandler(data)">
-            <span v-if="!items[data.index].isEdit">Editar</span>
+          <b-button
+            pill
+            variant="warning"
+            size="sm"
+            @click="editRowHandler(data)"
+          >
+            <span v-if="!tableItems[data.index].isEdit">Editar</span>
             <span v-else>Guardar</span>
           </b-button>
         </template>
       </b-table>
-      <!-- <pre>
-          {{ items }}
-        </pre> -->
     </div>
+    <!-- <pre>
+      {{ tableItems }}
+    </pre> -->
   </div>
 </template>
 <script>
-import axios from "axios";
-
 export default {
   name: "dataTableComponent",
   components: {},
-  created() { },
-  mounted() {
-    this.listarPersonas();
+  props: {
+    items: Array,
+    fields: Array,
   },
+
   data() {
     return {
-      fields: [
-        {
-          key: "id",
-          label: "ID",
-        },
-        {
-          key: "cedula",
-          label: "Cedula",
-        },
-        {
-          key: "nombre",
-          label: "Nombre",
-        },
-        {
-          key: "agencia",
-          label: "Agencia",
-        },
-        {
-          key: "celular",
-          label: "Celular",
-        },
-        {
-          key: "estado_personas_id",
-          label: "Estado",
-        },
-        {
-          key: "correo",
-          label: "Correo",
-        },
-        {
-          key: "grupo",
-          label: "Grupo",
-          type: "checkbox",
-        },
-        {
-          key: "rol_id",
-          label: "Rol",
-          type: "select",
-        },
-        { key: "edit", label: "" },
-      ],
-      items: null,
+      selectedRow: {},
+      tableItems: this.items,
     };
   },
   methods: {
-    async listarPersonas() {
-      const optionsRequest = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      axios
-        .get(`http://127.0.0.1:8000/api/persona`, {
-          optionsRequest,
-        })
-        .then((response) => {
-          this.items = response.data;
-          this.items = this.items.map((item) => ({ ...item, isEdit: false }));
-          console.log(this.items);
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-        .finally(function () {
-          console.log("lista de usuarios extraida con exito!");
-        });
-    },
     editRowHandler(data) {
-      this.items[data.index].isEdit = !this.items[data.index].isEdit;
+      this.tableItems[data.index].isEdit = !this.tableItems[data.index].isEdit;
     },
   },
 };
--- frontend/asamblea-virtual/src/views/Persona.vue
+++ frontend/asamblea-virtual/src/views/Persona.vue
@@ -1,20 +1,100 @@
-<script setup>
+<script>
 import TitleComponent from "../components/TitleComponent.vue";
 import dataTableComponent from "../components/dataTableComponent.vue";
+import axios from "axios";
+export default {
+  name: "Persona",
+  components: {
+    dataTableComponent,
+    TitleComponent,
+  },
+
+  created() {
+    this.listarPersonas();
+  },
+
+  data() {
+    return {
+      fields: [
+        {
+          key: "id",
+          label: "ID",
+        },
+        {
+          key: "cedula",
+          label: "Cedula",
+        },
+        {
+          key: "nombre",
+          label: "Nombre",
+        },
+        {
+          key: "agencia",
+          label: "Agencia",
+        },
+        {
+          key: "celular",
+          label: "Celular",
+        },
+        {
+          key: "estado_personas_id",
+          label: "Estado",
+        },
+        {
+          key: "correo",
+          label: "Correo",
+        },
+        {
+          key: "grupo",
+          label: "Grupo",
+          type: "checkbox",
+        },
+        {
+          key: "rol_id",
+          label: "Rol",
+          type: "select",
+        },
+        { key: "edit", label: "" },
+      ],
+      items: [],
+    };
+  },
+
+  methods: {
+    async listarPersonas() {
+      const url = "http://127.0.0.1:8000/api/persona";
+      const optionsRequest = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      axios
+        .get(url, optionsRequest)
+        .then((response) =>
+          response.data.map((row) => {
+            row.isEdit = false;
+            this.items.push(row);
+          })
+        )
+        .catch(console.log)
+        .finally(console.log);
+    },
+  },
+};
 </script>
 <template>
   <div class="children">
     <div id="contenido">
       <TitleComponent title="Personas" />
-      <dataTableComponent></dataTableComponent>
+      <dataTableComponent :items="items" :fields="fields"></dataTableComponent>
     </div>
   </div>
 </template>
 <style scoped>
-#contenido{
-
+#contenido {
   margin: auto;
-  width: 100% ;
-  
+  width: 100%;
 }
 </style>
